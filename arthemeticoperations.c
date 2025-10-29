56888607193179a24f917f28c22358aaf374dcc1
prachimahapatro2007-cyber <prachimahapatro2007@gmail.com>
2025-10-29 18:03:20 +0530

    Create arthemeticoperations.c

---
+++ arthemeticoperations.c
@@ -0,0 +1,19 @@
+#include<stdio. h>
+#include<math.h>
+int main()
+{
+int num1,num2;
+scanf("%d%d",&num1,&num2) ;
+int Sum=num1+num2;
+int Difference=num1-num2;
+int Product=num1*num2;
+int Quotient=num1/num2;
+int Remainder=num1%num2;
+printf("Sum:%dSum);
+printf ("Difference:%d",Difference);
+printf("Product:%d",Product);
+printf ("Quotient:%d",Quotient);
+printf ("Remainder:%d,Remainder) ;
+return 0;
+  }
+  
